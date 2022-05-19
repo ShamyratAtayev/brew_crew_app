47fb661b0e9ed1cf089a0d2eeafd88e966577d83
ShamyratAtayev <samurad1310@gmail.com>
2022-05-19 21:11:49 +0300

    Brew Crew

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -17,7 +17,7 @@ class Home extends StatelessWidget {
     void _showSettingsPanel() {
       showModalBottomSheet(context: context, builder: (context) {
         return Container(
-          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 60.0),
+          padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 60.0),
           child: SettingsForm(),
         );
       });
